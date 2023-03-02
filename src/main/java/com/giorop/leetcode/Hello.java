5d08343185cad33352649e8af4a96f2f28bd461f
giorop <xiayuren1993@gmail.com>
2023-03-02 08:23:20 +0800

    test commit

---
+++ src/main/java/com/giorop/leetcode/Hello.java
@@ -0,0 +1,14 @@
+package com.giorop.leetcode;
+
+/**
+ * @Description TODO
+ * @ClassName Hello
+ * @Author 夏雨人
+ * @DateTime 2023/3/2 7:15
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/com/giorop/leetcode/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Description TODO
+ * @ClassName package-info
+ * @Author 夏雨人
+ * @DateTime 2023/3/2 7:15
+ * @Version 1.0
+ */
+package com.giorop.leetcode;
